45887520bdb4956535dae0369201507482cbc3a1
Sandra Costa <sandrascsilva@fac.pe.senac.br>
2023-05-17 18:23:36 -0300

    Atualização do link server-json

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-   baseURL: "http://localhost:5000",
+   baseURL: "https://server-json-sch2.onrender.com",
 });
 
 export default api;
